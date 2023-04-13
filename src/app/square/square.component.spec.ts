f709fde06b773b5c0c858ff82e5615719d69283a
Yohannees <yohannesgetachewerieso@gmail.com>
2023-04-14 01:37:43 +0300

    nebula external ui kit added

--- src/app/square/square.component.spec.ts
+++ src/app/square/square.component.spec.ts
@@ -4,14 +4,10 @@ import { Component, Input } from '@angular/core';
   selector: 'app-square',
   template: `
     <button nbButton *ngIf="!value">{{ value }}</button>
-    <button nbButton hero status="success" *ngIf="value == 'X'">
-      {{ value }}
-    </button>
-    <button nbButton hero status="info" *ngIf="value == 'O'">
-      {{ value }}
-    </button>
+    <button nbButton hero status="success" *ngIf="value == 'X'">{{ value }}</button>
+    <button nbButton hero status="info" *ngIf="value == 'O'">{{ value }}</button>
   `,
-  styles: ['button { width: 100%; height: 100%; font-size: 5em !important; }'],
+  styles: ['button { width: 100%; height: 100%; font-size: 5em !important; }']
 })
 export class SquareComponent {
   @Input()
